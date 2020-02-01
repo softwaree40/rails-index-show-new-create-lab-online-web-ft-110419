1236687cd9f83b260ae01883557364b355cf1f67
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-01 16:52:16 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,10 +1,11 @@
 class CouponsController < ApplicationController
     def index 
+       
       @coupons = Coupon.all
-      render "coupons/index"
+     
     end
     def show 
-      @coupon = Coupon.find(params[:coupon_code],params[:store])
+      @coupon = Coupon.find(params[:id])
     end
     
     def new 
@@ -12,8 +13,9 @@ class CouponsController < ApplicationController
     end
     
     def create 
-       @coupon = Coupon.create(params[:coupon_code],params[:store])
-       redirect_to coupons_path
+       @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code],store: params[:coupon][:store])
+       
+       redirect_to coupon_path(@coupon.id)
     end
   
 end
---
+++ app/models/coupon.rb
@@ -0,0 +1,8 @@
+class Coupon < ActiveRecord::Base
+    # attr_accessor :coupon_code, :store
+    # def initialize(params)
+    #   @coupon_code = params[:coupon_code]
+    #   @store = params[:store]
+    # end
+  
+end
