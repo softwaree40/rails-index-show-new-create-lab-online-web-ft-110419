d7db7a26f01b3b859eba86b842148edc803a7664
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-01 19:55:38 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,7 +13,7 @@ class CouponsController < ApplicationController
     end
     
     def create 
-       @coupon = Coupon.create(coupon_code: params.require(coupons).permit(coupon_code,store)
+       @coupon = Coupon.create(params.require(:coupons).permit(coupon_code,store)
        
        redirect_to coupon_path(@coupon.id)
     end
