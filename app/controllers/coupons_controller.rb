5339e19323fbced1ce667bb02c90311d4e262085
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-01 20:03:20 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,7 +13,7 @@ class CouponsController < ApplicationController
     end
     
     def create 
-       @coupon = Coupon.create(params[:coupons][:coupon_code],params[:coupons][:store]
+       @coupon = Coupon.create(params[:coupons][:coupon_code],params[:coupons][:store])
        redirect_to coupon_path(@coupon.id)
     end
   
